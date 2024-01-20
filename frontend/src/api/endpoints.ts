c70ac49b456d9929d91091608d0b72d22bfaa197
Mako88 <afuturepilotis@gmail.com>
2024-01-19 22:50:11 -0600

    Use port 5051 in prod

--- frontend/src/api/endpoints.ts
+++ frontend/src/api/endpoints.ts
@@ -1,7 +1,7 @@
 import { GameFilter } from "../../../shared/enums";
 
 const host = import.meta.env.DEV ? "http://localhost" : "https://tracker.dxx-rebirth.com";
-const port = "5050";
+const port = import.meta.env.DEV ? "5050" : "5051";
 
 export const getGameCountEndpoint = (live: boolean, filter: GameFilter) =>
   `${host}:${port}/games/count/${live}/${filter}`;
