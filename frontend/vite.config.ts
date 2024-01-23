4295e7ad9ed5689bdab1134bb81b3222270d9026
Mako88 <afuturepilotis@gmail.com>
2024-01-22 23:39:40 -0600

    Update dependencies and remove frontend sourcemap

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: true,
     rollupOptions: {
       output: {
         entryFileNames: "assets/[name].js",
